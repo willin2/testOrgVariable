d3b70d6e928a2a90a0d4a55690cdf1b6cfdb3037
Will Lin <willlin@apjcorp.com>
2023-07-28 15:50:26 +0800

    no jq

--- .github/scripts/replace_variables.sh
+++ .github/scripts/replace_variables.sh
@@ -6,19 +6,33 @@ json_file="vars.json"
 cp $values_file ${values_file}.new
 
 curl -s -L \
-  -H "Accept: application/vnd.github+json" \
-  -H "Authorization: Bearer $MY_TOKEN" \
-  -H "X-GitHub-Api-Version: 2022-11-28" \
-  https://api.github.com/orgs/willin2/actions/variables > $json_file
+    -H "Accept: application/vnd.github+json" \
+    -H "Authorization: Bearer $MY_TOKEN" \
+    -H "X-GitHub-Api-Version: 2022-11-28" \
+    https://api.github.com/orgs/willin2/actions/variables > $json_file
 
-num=`cat $json_file |jq ".variables |length"`
-for ((i=0; i<$num; i++))
+# # jq
+# num=`cat $json_file |jq ".variables |length"`
+# for ((i=0; i<$num; i++))
+# do
+#     name=`cat $json_file |jq .variables[$i].name |sed 's/"//g'`
+#     value=`cat $json_file |jq .variables[$i].value |sed 's/"//g'`
+
+#     if grep -q "<\$$name>" $values_file
+#     then
+#         sed -i "s|<\$$name>|$value|" ${values_file}.new
+#     fi
+# done
+
+# non jq
+num=`grep "\"name\":" $json_file |wc -l`
+for ((i=1; i<=$num; i++))
 do
-        name=`cat $json_file |jq .variables[$i].name |sed 's/"//g'`
-        value=`cat $json_file |jq .variables[$i].value |sed 's/"//g'`
+    name=`grep "\"name\":" $json_file |sed -n "${i}p" |awk -F\" '{print $4}'`
+    value=`grep "\"value\":" $json_file |sed -n "${i}p" |awk -F\" '{print $4}'`
 
-        if grep -q "<\$$name>" $values_file
-        then
-                sed -i "s|<\$$name>|$value|" ${values_file}.new
-        fi
+    if grep -q "<\$$name>" $values_file
+    then
+        sed -i "s|<\$$name>|$value|" ${values_file}.new
+    fi
 done
